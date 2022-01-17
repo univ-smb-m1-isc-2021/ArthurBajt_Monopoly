81b11d7650c2a194e393eed06e3033322f9b8645
Léo <58537513+leolaf@users.noreply.github.com>
2022-01-17 14:08:37 +0100

    Update TestJoueur.java

--- src/test/java/TestJoueur.java
+++ src/test/java/TestJoueur.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestJoueur {
-/*
+
     @Test
     public void TestCreateJoueur(){
         Joueur j = new Joueur("Michel");
         assertEquals("Michel", j.getNom());
     }
-    */
+    
 }
